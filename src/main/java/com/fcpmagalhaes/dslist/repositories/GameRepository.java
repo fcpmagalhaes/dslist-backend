25f265f92decff3d5f983747aa4df349f2bb7b7b
Felipe Magalhaes <felipe.magalhaes@valtech.com>
2024-11-12 22:06:12 -0300

    Create game repository with jpa

---
+++ src/main/java/com/fcpmagalhaes/dslist/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.fcpmagalhaes.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fcpmagalhaes.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long>{
+
+}
