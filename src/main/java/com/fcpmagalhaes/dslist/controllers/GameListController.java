e25809538159519b28d89994d537fbdbf52fb776
Felipe Magalhaes <felipe.magalhaes@valtech.com>
2024-11-19 01:28:32 -0300

    Create endpoint to find by list

--- src/main/java/com/fcpmagalhaes/dslist/controllers/GameListController.java
+++ src/main/java/com/fcpmagalhaes/dslist/controllers/GameListController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fcpmagalhaes.dslist.dto.GameListDTO;
+import com.fcpmagalhaes.dslist.dto.GameMinDTO;
 import com.fcpmagalhaes.dslist.services.GameListService;
+import com.fcpmagalhaes.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "/lists")
@@ -17,13 +20,16 @@ public class GameListController {
 	@Autowired
 	private GameListService gameListService;
 	
-//	@GetMapping(value = "/{id}")
-//	public GameDTO findById(@PathVariable Long id) {
-//		return gameService.findById(id);
-//	}
+	@Autowired
+	private GameService gameService;
 	
 	@GetMapping
 	public List<GameListDTO> findAll() {
 		return gameListService.findAll();
+	}
+	
+	@GetMapping(value = "/{listId}/games")
+	public List<GameMinDTO> findByListId(@PathVariable Long listId) {
+		return gameService.findByList(listId);
 	}
 }
