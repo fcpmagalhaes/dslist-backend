51edc0a52f4fc7a6f802efb92834f425320564ff
Felipe Magalhaes <felipe.magalhaes@valtech.com>
2024-11-19 01:27:56 -0300

    Add method to find games by list

--- src/main/java/com/fcpmagalhaes/dslist/services/GameService.java
+++ src/main/java/com/fcpmagalhaes/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.fcpmagalhaes.dslist.dto.GameDTO;
 import com.fcpmagalhaes.dslist.dto.GameMinDTO;
 import com.fcpmagalhaes.dslist.entities.Game;
+import com.fcpmagalhaes.dslist.projections.GameMinProjection;
 import com.fcpmagalhaes.dslist.repositories.GameRepository;
 
 @Service
@@ -29,6 +30,9 @@ public class GameService {
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 	
-	
-
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
 }
